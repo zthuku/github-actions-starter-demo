8f31eb34a4035bf7c88a8d2c316c60bcc01029de
zthuku <zakayo@backbase.com>
2022-11-01 09:13:17 +0300

    chore: Use Terraform Actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=1.3.3"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = ">=2.0"
     }
   }
